1b887a3a6e75721454f13b9a83017656bdce5d9c
Luke <lsh-0@users.noreply.github.com>
2019-06-14 12:25:13 +0930

    adds uwsgi to dependencies

--- install.sh
+++ install.sh
@@ -2,7 +2,18 @@
 set -e
 . mkvenv.sh
 source venv/bin/activate
-pip install -r requirements.lock
+
+echo "installing ..."
+if [ -e requirements.lock ]; then
+    # remove the .lock file when you want to recreate it
+    pip install -r requirements.lock
+else
+    pip install -r requirements.txt
+    echo "locking..."
+    pip freeze > requirements.lock
+    echo "wrote 'requirements.lock'"
+fi
+
 if [ ! -e app.cfg ]; then
     ln -s elife.cfg app.cfg
 fi
