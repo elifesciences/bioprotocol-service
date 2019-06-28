1c2e962fd72a040c5ba04f17b82b8f8577423558
Luke <lsh-0@users.noreply.github.com>
2019-06-28 12:15:57 +0930

    updated example post with username/password requisites

--- example/example-post.sh
+++ example/example-post.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -eu
 
+username="$1"
+password="$1"
+
 function send {
     payload="$1"
     echo "=> $payload"
     curl \
         -d "@$payload" \
         -X POST \
+        -u "$username:$password" \
         -H "Content-Type: application/json" \
         -w " (HTTP %{http_code})" \
         https://ci--bp.elifesciences.org/bioprotocol/article/12345 
