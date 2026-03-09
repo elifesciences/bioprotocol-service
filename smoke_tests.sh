f4381de472c698ebeb26cd89887168885970ec00
Scott Aubrey <scott@aubrey.org.uk>
2026-03-09 10:57:56 +0000

    Fix pipeline for CI environment
    https://github.com/elifesciences/issues/issues/9497

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -3,7 +3,11 @@ set -eux
 
 function hit {
     path="$1"
-    test $(curl --write-out %{http_code} --silent --output /dev/null https://$(hostname)$path) = 200
+    hostname=$(hostname)
+    if [[ "$hostname" == "ci--bp.elifesciences.org" ]]; then
+        hostname="ci-bp.elifesciences.org"
+    fi
+    test $(curl --write-out %{http_code} --silent --output /dev/null https://$hostname$path) = 200
 }
 
 hit /ping
